11d1e3e85edc38cf71b0e2789462a1a2937a159c
Yli <salazarylizanicole@gmail.com>
2022-11-19 10:37:11 +0800

    Adding Instructions

---
+++ Index.py
@@ -0,0 +1,4 @@
+#Search for a program that interest you in youtune
+#picked project "https://youtu.be/CKmAZss-T5Y"
+#this project uses ai to identify gestures that commands the presentation
+#Recreate the project 
